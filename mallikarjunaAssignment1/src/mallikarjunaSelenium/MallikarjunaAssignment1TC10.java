5dfd56e4c327a8cb3ac2b9d298d3da3b7781e745
lenovo <lenovo@DESKTOP-6K6D6KB>
2020-04-07 16:59:12 +0530

    Removed

--- mallikarjunaAssignment1/src/mallikarjunaSelenium/MallikarjunaAssignment1TC10.java
+++ mallikarjunaAssignment1/src/mallikarjunaSelenium/MallikarjunaAssignment1TC10.java
@@ -1,17 +1,15 @@
 package mallikarjunaSelenium;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+
 
 public class MallikarjunaAssignment1TC10 {
 
